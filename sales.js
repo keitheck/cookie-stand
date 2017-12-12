90e6f9082e3860caa86f78a696b5a1de71f882be
keitheck <eckertkeith@gmail.com>
2017-12-11 16:44:34 -0800

    work in progress

---
+++ sales.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
+
+var pike = {
+  custMin: 23,
+  custMax: 65,
+  avgCookies: 6.3,
+  avgCustHour: function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (this.custMax - this.custMin) + this.custMin);
+  },
+  cookiesTotal: function () {
+    var cookiesPerHour = [];
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesPerHour.push(Math.round(this.avgCookies * this.avgCustHour()));
+    };
+    return cookiesPerHour;
+  }
+};
+//console.log(pike.cookiesTotal());
+var cookiesTotalArr = pike.cookiesTotal();
+console.log(cookiesTotalArr);
+
+
+
+
+// function calcCookies() {
+//   var cookiesPerHour = [];
+//
+//   for (var i = 0; i < storeHours.length; i++) {
+//     cookiesPerHour.push(Math.round(pike.avgCookies * pike.avgCustHour()));
+//   };
+// console.log("cookiesPerHour:", cookiesPerHour);
+//
+// }
+// calcCookies();
